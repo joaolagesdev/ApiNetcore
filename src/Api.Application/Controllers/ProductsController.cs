a335f3b58062998715f5c8c8b9c9c15c299bcd70
João <joaolages.dev@gmail.com>
2021-05-12 15:28:14 -0300

    Api.Application - Criação do método get por id

--- src/Api.Application/Controllers/ProductsController.cs
+++ src/Api.Application/Controllers/ProductsController.cs
@@ -10,15 +10,17 @@ namespace Application.Controllers
     [ApiController]
     public class ProductsController : ControllerBase
     {
-        //private IProductService _service;
-        //public ProductsController(IProductService service)
-        //{
-        //    _service = service;
-        //}
+        private IProductService _service;
+        public ProductsController(IProductService service)
+        {
+            _service = service;
+        }
+
+        // public async Task<ActionResult> GetAll() { }
+        // public async Task<ActionResult> GetAll([FromServices] IProductService service)
 
         [HttpGet]
-        // public async Task<ActionResult> GetAll() { }
-        public async Task<ActionResult> GetAll([FromServices] IProductService service)
+        public async Task<ActionResult> GetAll()
         {
             if (!ModelState.IsValid)
             {
@@ -27,13 +29,32 @@ namespace Application.Controllers
 
             try
             {
-                // return Ok(await service.GetAll());
-                return Ok(await service.GetAll());
+                return Ok(await _service.GetAll());
             }
-            catch (ArgumentException e) // Tratar erros de controllers
+            catch (ArgumentException e) 
             {
                 return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
             }
         }
+
+        [HttpGet]
+        [Route("{id}", Name = "GetById")]
+        public async Task<ActionResult> Get(Guid id)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            try
+            {
+                return Ok(await _service.Get(id));
+            }
+            catch (ArgumentException e)
+            {
+                return StatusCode((int)HttpStatusCode.InternalServerError, e.Message);
+            }
+        }
+
     }
 }
